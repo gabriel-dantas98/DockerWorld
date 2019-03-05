ac33b1c006fecaaaa89e15074c7a3c88da94c3f5
Gabriel Dantas <gabsdevops@gmail.com>
2019-03-05 20:43:04 -0300

    feat(ci/cd): Change heroku-cicd base image and add healthcheck script

---
+++ heroku-cicd/healthcheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check the health of the web service every thirty-seconds
+# for up to ten minutes, until it responds with HTTP status 200.
+# REF: https://devcenter.heroku.com/articles/using-terraform-with-heroku#deploying-code-to-an-app
+# command: healthcheck.sh https://gb-church-api.herokuapp.com/healthcheck
+
+fail_count=1
+while true
+do
+  http_status=$(curl --write-out %{http_code} --silent --output /dev/null $1)
+
+  if [ "$http_status" -eq "200" ]; then
+    echo "$(date -u) health check succeeded to $1"
+    exit 0
+  else
+    if [ "$fail_count" -eq "21" ]; then
+      echo "$(date -u) health check failed (status $http_status) to $1"
+      exit 2
+    else
+      echo "$(date -u) health check ${fail_count}/20 to $1"
+      sleep 30
+      fail_count=$[$fail_count +1]
+    fi
+  fi
+done
